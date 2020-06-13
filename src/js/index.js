88a326d2fc37aa5834d62a9be4ea4271e35c0767
Piotr <piotr-gorka@wp.pl>
2020-06-13 19:38:40 +0100

    simple editor js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,27 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-/* place your code below */
+console.log("HELLO 🚀");
 
-console.log('HELLO 🚀')
+
+
+const buttonLoad = document.querySelector('.load--js');
+const buttonSave = document.querySelector('.save--js');
+let entryArea = document.querySelector('textarea');
+
+const entry = localStorage.getItem('entry');
+
+let result = "";
+
+if (entry) {
+  result = entry;
+}
+
+entryArea.innerHTML = result;
+
+buttonLoad.addEventListener("click", () => {
+  entryArea.value = entry;
+});
+
+buttonSave.addEventListener("click", () => {
+  localStorage.setItem("entry", entryArea.value);
+});
